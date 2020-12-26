34f006bc652e1c528bf631781af4aa0cadf86a76
Jedrzej Kopiszka <jedrzej.kopiszka2@gmail.com>
2020-12-26 01:10:27 +0100

    Delete feature_1.py

--- feature_1.py
+++
@@ -1,3 +0,0 @@
-print("First feature is added to the feature_1.py")
-print("LinkedIn is a very interesting social media platform for specialists")
-print("Now I change something and that shit still works")
